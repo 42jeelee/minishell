b719aafa173a1c6b3ec5b70177fd625da25751b1
Jeehoon Lee <jeelee@c6r3s7.42seoul.kr>
2023-04-30 22:20:26 +0900

    minishell v1

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 21:38:43 by jeelee            #+#    #+#             */
-/*   Updated: 2023/04/30 21:36:04 by jeelee           ###   ########.fr       */
+/*   Updated: 2023/04/30 22:18:28 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <stdlib.h>
+# include <sys/wait.h>
+# include <fcntl.h>
 # include "./libft/libft.h"
 
 typedef struct s_arg
@@ -34,6 +36,15 @@ typedef struct s_cmds
 	struct s_cmds	*next;
 	struct s_cmds	*prev;
 }	t_cmds;
+
+typedef struct s_execute_arg
+{
+	int		**pfd;
+	int		fd[2];
+	pid_t	*pid;
+	int		i;
+	char	*cmd_path;
+}	t_execute_arg;
 
 t_cmds	*new_prompt(int *num_of_cmd);
 
@@ -54,6 +65,23 @@ t_arg	*get_arg(int num_of_cmd, char **paths);
 void	free_words(char **words);
 void	free_cmds(t_cmds *cmds);
 void	free_arg(t_arg *arg);
+
+int		**make_pipes(int num_of_pipe);
+int		print_perror(char *str);
+void	close_pipes_exept(int **pfd, int num_of_pipe, int *fd);
+size_t	ft_strlen(const char *str);
+char	*cmd_abs_path(char *cmd, char **paths);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+int		exe_cmd_line(t_arg *arg, t_cmds *cmds, char **env);
+void	setting(t_arg *arg, t_cmds *cmds);
+
+int		init(t_arg *arg, t_execute_arg *exe_arg);
+void	pipe_redir(int **pfd, int i, int num_of_cmd);
+void	in_redir(t_cmds *cmds, int *fd);
+void	out_redir(t_cmds *cmds, int *fd);
+int		is_relative_path(char *cmd);
+void	i_hate_25_line(t_execute_arg *exe_arg, t_arg *arg, t_cmds *cmds);
 
 // PRINT
 void	print_cmds(t_arg *arg, t_cmds *cmds);
