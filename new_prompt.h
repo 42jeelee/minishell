aa2e7d6541e570fc124a7e8afad56aa3ca5e31c1
JEEHOON LEE <hoon@JEEHOONui-MacBookPro.local>
2023-05-06 00:07:43 +0900

    sep new_prompt header

---
+++ new_prompt.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   new_prompt.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/06 00:04:34 by jeelee            #+#    #+#             */
+/*   Updated: 2023/05/06 00:06:19 by jeelee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef NEW_PROMPT_H
+# define NEW_PROMPT_H
+
+# include "common.h"
+
+t_cmds	*new_prompt(int *num_of_cmd);
+
+int		parse_line(char *line, t_cmds **cmds, int *num_of_cmd);
+
+t_cmds	*new_cmd(char *commad);
+void	add_cmd_list(t_cmds *new, t_cmds **list);
+
+int		add_list_word(char *word, char ***list);
+int		add_list_int(int num, int **list);
+int		create_word_list(char ***list);
+int		create_int_list(int **list);
+int		init_cmd(t_cmds *cmd);
+
+char	**get_path(char **env);
+t_arg	*get_arg(int num_of_cmd, char **paths);
+
+void	free_words(char **words);
+void	free_cmds(t_cmds *cmds);
+void	free_arg(t_arg *arg);
+
+int		is_in_idx(char *str, char *catch);
+char	*get_wordcatch(char *str, char *catch);
+int		is_builtin(char *word);
+
+#endif
