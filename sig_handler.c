18d3be52ca73ed80301c0da04291e5cfa20c88a4
Jeehoon Lee <jeelee@c4r5s6.42seoul.kr>
2023-05-22 16:41:57 +0900

    remove sig_handler \'have_child\'

--- sig_handler.c
+++ sig_handler.c
@@ -6,7 +6,7 @@
 /*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 17:41:16 by jeelee            #+#    #+#             */
-/*   Updated: 2023/05/20 14:51:21 by byejeon          ###   ########.fr       */
+/*   Updated: 2023/05/22 16:40:54 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,4 @@ void	sig_handler(int signum)
 		rl_on_new_line();
 		rl_redisplay();
 	}
-}
-
-void	sig_have_child(int signum)
-{
-	if (signum == SIGINT)
-		ft_putchar_fd('\n', STDOUT_FILENO);
 }
