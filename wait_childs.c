a56f3b83ffcc1a0458b5d720a6a8430c3b1b826d
Jeehoon Lee <jeelee@c5r9s1.42seoul.kr>
2023-05-24 15:54:22 +0900

    only signal SIGINT and SIGQUIT has newline

--- wait_childs.c
+++ wait_childs.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   wait_childs.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: byejeon <byejeon@student.42seoul.k>        +#+  +:+       +#+        */
+/*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 17:43:29 by byejeon           #+#    #+#             */
-/*   Updated: 2023/05/22 19:47:32 by byejeon          ###   ########.fr       */
+/*   Updated: 2023/05/24 15:50:44 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,9 @@ void	wait_childs(t_execute_arg *exe_arg)
 		{
 			if (WTERMSIG(exe_arg->exit_code) == SIGQUIT)
 				ft_putstr_fd("QUIT: 3", STDOUT_FILENO);
+			if (WTERMSIG(exe_arg->exit_code) == SIGINT || WTERMSIG(exe_arg->exit_code) == SIGQUIT)
+				ft_putchar_fd('\n', STDOUT_FILENO);
 			exe_arg->exit_code |= (128 + WTERMSIG(exe_arg->exit_code)) << 8;
-			ft_putchar_fd('\n', STDOUT_FILENO);
 		}
 	}
 }
