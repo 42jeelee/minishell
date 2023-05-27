a44fdcecbb1adcc69d3baea16bd50761ec83d0cd
JEEHOON LEE <hoon@JEEHOONui-MacBookPro.local>
2023-05-28 02:06:27 +0900

    add SHLVL

--- envvalue.c
+++ envvalue.c
@@ -6,35 +6,34 @@
 /*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 20:37:36 by jeelee            #+#    #+#             */
-/*   Updated: 2023/05/21 21:19:50 by jeelee           ###   ########.fr       */
+/*   Updated: 2023/05/28 02:04:38 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	set_valueenv(char *key, char *value, char **env)
+int	set_valueenv(char *key, char *value, char ***env)
 {
 	int		i;
-	int		key_size;
 	char	*tmp;
 
-	if (!key || !key)
+	if (!key || !(key[0]))
 		return (0);
 	if (!value)
 		value = "";
-	key_size = ft_strlen(key);
-	i = -1;
-	while (env[++i])
+	tmp = ft_tristrjoin(key, "=", value);
+	if (!tmp)
+		return (1);
+	i = find_key_env(key, *env);
+	if (i == -1)
 	{
-		if (!ft_strncmp(key, env[i], key_size))
-		{
-			tmp = ft_tristrjoin(key, "=", value);
-			if (!tmp)
-				return (1);
-			free(env[i]);
-			env[i] = tmp;
-			break ;
-		}
+		if (add_list_word(tmp, env))
+			return (1);
+	}
+	else
+	{
+		free((*env)[i]);
+		(*env)[i] = tmp;
 	}
 	return (0);
 }
--- new_prompt.h
+++ new_prompt.h
@@ -6,7 +6,7 @@
 /*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 00:04:34 by jeelee            #+#    #+#             */
-/*   Updated: 2023/05/27 19:11:02 by jeelee           ###   ########.fr       */
+/*   Updated: 2023/05/28 01:08:15 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int		init_cmd(t_cmds *cmd);
 int		get_block_size(char *word, char c);
 int		change_block_env(char **word, t_blockinfo *bi, t_arg *env);
 
-int		set_valueenv(char *key, char *value, char **env);
+int		set_valueenv(char *key, char *value, char ***env);
 char	*get_value_env(char *key, int size, char **env);
 
 char	**make_envlist(char **env);
